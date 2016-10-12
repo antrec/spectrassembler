5635ba77bda9784e09a0335a64531cd5c19926da
antrec <antoine.recanati@gmail.com>
2016-10-12 16:38:58 +0200

    corrected pipeline.sh example

--- spectrassembler_pipeline.sh
+++ spectrassembler_pipeline.sh
@@ -28,7 +28,7 @@ LEN_THR=3500
 VERB_OPT="-vv"
 
 # READS (FASTA FILE)
-if [$# != 1]; then
+if [ $# != 1 ]; then
   echo "PLEASE PASS ARGUMENT TO THE SCRIPT :
   PATH/TO/FASTAFILE.FASTA (file containing the raw reads)."
   exit
@@ -36,29 +36,29 @@ fi
 READS_FILE=$1
 
 # Sanity checks
-if [${#POA} == 0]; then
+if [ ${#POA} == 0 ]; then
   echo "poa not in PATH. Please specify manually the PATH
   to the executable poa inside the shell script (l.15, instead of POA=`which poa`)!"
 fi
-if[${#MINIMAP} == 0]; then
+if[ ${#MINIMAP} == 0 ]; then
   echo "minimap not in PATH. Please specify manually the PATH
   to the executable minimap inside the shell script (l.16, instead of POA=`which poa`)!"
 fi
 # option to be passed to python script
 WRITE_POA_FILES_OPT=""
-if ["$DO_CONSENSUS" = true]; then
+if [ "$DO_CONSENSUS" == true ]; then
   WRITE_POA_FILES_OPT="-w"
 fi
 
 
 # Compute alignments with minimap
-minimap -S $READS_FILE $READS_FILE > overlaps.mini
+$MINIMAP -S $READS_FILE $READS_FILE > overlaps.mini
 
 # Compute consensus
 python $SRC_DIR/spectral_layout_from_minimap.py -f $READS_FILE -m overlaps.mini -r $WRK_DIR $WRITE_POA_FILES_OPT $VERB_OPT --w_len $W_LEN --w_ovl_len $W_OVL_LEN --sim_thr $SIM_THR --len_thr $LEN_THR
 
 # COMPUTE CONSENSUS sequences
-if ["$DO_CONSENSUS" == true]; then
+if [ "$DO_CONSENSUS" == true ]; then
   NUM_CC=`find $WRK_DIR/cc_* -type d | wc -l`
   SCORE_MAT="$SRC_DIR/poa-score.mat"
   for cc_idx in $(seq 0 $NUM_CC);
@@ -66,13 +66,13 @@ if ["$DO_CONSENSUS" == true]; then
     cd $WRK_DIR/cc_$cc_idx
     for file in poa_in_cc_*.fasta
     do
-      while [`jobs | wc -l` -ge $NUM_NODES ]
+      while [ `jobs | wc -l` -ge $NUM_NODES ]
       do
         sleep 2
       done
-      if [! test -f $file.clustal.out] #Do not recompute the same thing twice in case you stopped a computation earlier.
+      if ! test -f $file.clustal.out #Do not recompute the same thing twice in case you stopped a computation earlier.
       then
-        poa -read_fasta $file -clustal $file.clustal.out -hb $SCORE_MAT &
+        $POA -read_fasta $file -clustal $file.clustal.out -hb $SCORE_MAT &
       fi
     done
     cd ../
@@ -81,7 +81,7 @@ if ["$DO_CONSENSUS" == true]; then
 fi
 
 # Clean up if asked
-if ["$CLEAN_TEMP_FILES" == true]; then
+if [ "$CLEAN_TEMP_FILES" == true ]; then
   for cc_idx in $(seq 0 $NUM_CC);
   do
     rm -r $WRK_DIR/cc_$cc_idx
